a99cb3809a6991a38e95e466a277b640c00d7112
Konstantin Senkevich <konstsen@gmail.com>
2021-01-26 18:34:05 +0300

    Code refactoring: method for message signing and etc.

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,9 @@ use super::models;
 use super::extractor;
 use super::coin;
 
+const BAD_URL: &'static str = "Unable to modify path of url";
+const BAD_BODY: &'static str = "Bad body. Failed to get bytes.";
+
 pub struct BTCClient<TConnector> {
     client: hyper::Client<TConnector>,
     auth_context: context::AuthContext,
@@ -19,9 +22,6 @@ where
     const PUBLIC: &'static str = "public";
     const SYMBOL: &'static str = "symbol";
 
-    const BAD_URL: &'static str = "Unable to modify path of url";
-    const BAD_BODY: &'static str = "Bad body. Failed to get bytes.";
-
     pub fn new(
         client: hyper::Client<TConnector>,
         auth_context: context::AuthContext,
@@ -35,7 +35,7 @@ where
     pub async fn get_all_symbols(&self) -> Option<models::Symbols> {
         let mut url = self.auth_context.base_url.clone();
         url.path_segments_mut()
-            .expect(Self::BAD_URL)
+            .expect(BAD_URL)
             .push(Self::PUBLIC)
             .push(Self::SYMBOL);
         let request = hyper::Request::builder()
@@ -55,32 +55,16 @@ where
     pub async fn get_balance(&self) -> Option<models::Balance> {
         let mut url = self.auth_context.base_url.clone();
         url.path_segments_mut()
-            .expect(Self::BAD_URL)
+            .expect(BAD_URL)
             .push(Self::ACCOUNT)
             .push(Self::BALANCE);
-        let method = hyper::Method::GET;
-        let body = hyper::Body::empty();
-        let body = hyper::body::to_bytes(body)
-            .await
-            .expect(Self::BAD_URL);
-        let body = String::from_utf8(body.to_vec())
-            .expect("Body must be valid UTF-8");
-        let timestamp = chrono::Utc::now().timestamp().to_string();
-        let path_with_query = &url[url::Position::BeforePath..];
-        let message = get_message(method.clone(), &timestamp, path_with_query, &body);
-        let jwt = self.auth_context.sign(message, timestamp);
-        let request = hyper::Request::builder()
-            .header("Accept", "application/json")
-            .header("Authorization", jwt)
-            .uri(url.to_string())
-            .method(method)
-            .body(hyper::Body::empty())
-            .expect("Failed to build request!");
-        let response = self.client.request(request)
-            .await
-            .unwrap();
-        log::info!("{:#?}", response);
-        let body = response.into_body();
+        let (header, body) = process(
+            &self.client,
+            &self.auth_context,
+            url,
+            hyper::Method::GET,
+            hyper::Body::empty()).await;
+        log::info!("Header: {:#?}", header);
         extractor::extract_balance(body).await
     }
 
@@ -90,35 +74,19 @@ where
     ) -> Option<models::Orders> {
         let mut url = self.auth_context.base_url.clone();
         url.path_segments_mut()
-            .expect(Self::BAD_URL)
+            .expect(BAD_URL)
             .push(Self::ORDER);
         if let Some(coins) = coins {
             url.query_pairs_mut()
                 .append_pair("symbol", &coins.to_string());
         }
-        let method = hyper::Method::GET;
-        let body = hyper::Body::empty();
-        let body = hyper::body::to_bytes(body)
-            .await
-            .expect(Self::BAD_URL);
-        let body = String::from_utf8(body.to_vec())
-            .expect("Body must be valid UTF-8");
-        let timestamp = chrono::Utc::now().timestamp().to_string();
-        let path_with_query = &url[url::Position::BeforePath..];
-        let message = get_message(method.clone(), &timestamp, path_with_query, &body);
-        let jwt = self.auth_context.sign(message, timestamp);
-        let request = hyper::Request::builder()
-            .header("Accept", "application/json")
-            .header("Authorization", jwt)
-            .uri(url.to_string())
-            .method(method)
-            .body(hyper::Body::empty())
-            .expect("Failed to build request!");
-        let response = self.client.request(request)
-            .await
-            .unwrap();
-        log::info!("{:#?}", response);
-        let body = response.into_body();
+        let (header, body) = process(
+            &self.client,
+            &self.auth_context,
+            url,
+            hyper::Method::GET,
+            hyper::Body::empty()).await;
+        log::info!("Header: {:#?}", header);
         extractor::extract_orders(body).await
     }
 }
@@ -130,4 +98,36 @@ fn get_message(
     body: &str,
 ) -> String {
     format!("{}{}{}{}", method, timestamp, path_with_query, body)
+}
+
+async fn process<TConnector>(
+    client: &hyper::Client<TConnector>,
+    auth_context: &context::AuthContext,
+    url: url::Url,
+    method: hyper::Method, 
+    body: hyper::Body,
+) -> (http::response::Parts, hyper::Body) 
+where
+    TConnector: hyper::client::connect::Connect + Send + Sync + Clone + 'static
+{
+    let body = hyper::body::to_bytes(body)
+        .await
+        .expect(BAD_BODY);
+    let body = String::from_utf8(body.to_vec())
+        .expect("Body must be valid UTF-8");
+    let timestamp = chrono::Utc::now().timestamp().to_string();
+    let path_with_query = &url[url::Position::BeforePath..];
+    let message = get_message(method.clone(), &timestamp, path_with_query, &body);
+    let jwt = auth_context.sign(message, timestamp);
+    let request = hyper::Request::builder()
+        .header("Accept", "application/json")
+        .header("Authorization", jwt)
+        .uri(url.to_string())
+        .method(method)
+        .body(hyper::Body::empty())
+        .expect("Failed to build request!");
+    client.request(request)
+        .await
+        .unwrap()
+        .into_parts()
 }
